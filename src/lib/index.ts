dfcea58c571db31828eaf2bf3ab8c7c2fb8c4b70
mrx <1320504268@qq.com>
2020-10-19 21:56:00 +0800

    npm

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export { default as Switch } from './Switch/Switch.vue'
+export { default as Button } from './Button/Button.vue'
+export { default as Tabs } from './Tabs/Tabs.vue'
+export { default as Dialog } from './Dialog/Dialog.vue'
+export { openDialog as openDialog } from './Dialog/openDialog'
